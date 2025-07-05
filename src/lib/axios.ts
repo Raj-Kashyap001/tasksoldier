fe2b3f897fa7b55ef3012dd1cb53380c2e74b764
Raj Kashyap <rajkashyapatservice@outlook.com>
2025-07-05 20:16:01 +0530

    api path fix

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL:
-    process.env.NEXT_PUBLIC_API_BASE_URL || "https://localhost:3000/api/v1",
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    "https://tasksoldier.vercel.app/api/v1/",
   timeout: 10000,
 });
